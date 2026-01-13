79a02dd382a496c60ba161ea172a3d2963b2e011
Mikey Sleevi <development@frogman.simplelogin.com>
2026-01-12 17:18:17 -0700

    Adds support for Sitemaps (#5)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 import pagefind from "astro-pagefind";
 import { remarkReadingTime } from "./src/remark-reading-time";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [pagefind()],
+	site: "https://fraugsleeves.dev",
+	integrations: [pagefind(), sitemap()],
 	build: {
 		format: "file",
 	},
