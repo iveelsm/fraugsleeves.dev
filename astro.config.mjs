0de2b30dcdbd80783eec9a29f716348d3c9068b9
Mikey Sleevi <development@frogman.simplelogin.com>
2026-01-19 11:37:38 -0700

    adjust icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,41 @@ export default defineConfig({
 						type: "element",
 						tagName: "span",
 						properties: { className: ["anchor-icon"] },
-						children: [{ type: "text", value: "#" }],
+						children: [
+							{
+								type: "element",
+								tagName: "svg",
+								properties: {
+									xmlns: "http://www.w3.org/2000/svg",
+									width: "20",
+									height: "20",
+									viewBox: "0 0 24 24",
+									fill: "none",
+									stroke: "currentColor",
+									strokeWidth: "2",
+									strokeLinecap: "round",
+									strokeLinejoin: "round",
+								},
+								children: [
+									{
+										type: "element",
+										tagName: "path",
+										properties: {
+											d: "M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71",
+										},
+										children: [],
+									},
+									{
+										type: "element",
+										tagName: "path",
+										properties: {
+											d: "M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71",
+										},
+										children: [],
+									},
+								],
+							},
+						],
 					},
 				},
 			],
