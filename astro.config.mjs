4ea360ef7dda193330463f36802198887d0a30df
Mikey Sleevi <development@frogman.simplelogin.com>
2026-01-18 20:55:11 -0700

    Links in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import pagefind from "astro-pagefind";
 import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from "./src/remark-reading-time";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // eslint-disable-next-line
 const site = process.env.SITE_URL || "https://fraugsleeves.dev";
@@ -16,6 +18,25 @@ export default defineConfig({
 	},
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
+		rehypePlugins: [
+			rehypeSlug,
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: "prepend",
+					properties: {
+						className: ["heading-anchor"],
+						ariaLabel: "Link to this section",
+					},
+					content: {
+						type: "element",
+						tagName: "span",
+						properties: { className: ["anchor-icon"] },
+						children: [{ type: "text", value: "#" }],
+					},
+				},
+			],
+		],
 	},
 	experimental: {
 		svgo: true,
