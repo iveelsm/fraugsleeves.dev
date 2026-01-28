dd8376e37eb29089e457ec3844cd8e0c5996d6d8
Mikey Sleevi <development@frogman.simplelogin.com>
2026-01-28 10:51:51 -0700

    getting closer

--- src/components/wayfinders/index.ts
+++ src/components/wayfinders/index.ts
@@ -1,4 +1,7 @@
 import RecentWayfinders from "./recentWayfinders.astro";
 import WayfindersGrid from "./wayfindersGrid.astro";
 
-export { RecentWayfinders, WayfindersGrid };
+export {
+	RecentWayfinders,
+	WayfindersGrid
+};
