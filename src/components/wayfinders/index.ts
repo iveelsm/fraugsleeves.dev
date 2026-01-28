6ffb9725b5fba7c2c3fb7620d61604d21bada97d
Mikey Sleevi <development@frogman.simplelogin.com>
2026-01-28 10:52:20 -0700

    linting

--- src/components/wayfinders/index.ts
+++ src/components/wayfinders/index.ts
@@ -1,7 +1,4 @@
 import RecentWayfinders from "./recentWayfinders.astro";
 import WayfindersGrid from "./wayfindersGrid.astro";
 
-export {
-	RecentWayfinders,
-	WayfindersGrid
-};
+export { RecentWayfinders, WayfindersGrid };
