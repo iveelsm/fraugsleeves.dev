a300028457c9646cb887042a958ab87ce2cb50db
Mikey Sleevi <development@frogman.simplelogin.com>
2026-02-01 16:20:29 -0700

    stylesheet and rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 import { DEFAULT_DESCRIPTION, DEFAULT_TITLE } from "../constants";
+import { formatDate } from "../components/formatDate";
 
 export async function GET(context) {
   const blog = await getCollection('blog');
@@ -10,9 +11,10 @@ export async function GET(context) {
     description: DEFAULT_DESCRIPTION,
     site: context.site,
 	trailingSlash: false,
+    stylesheet: '/rss.xsl',
     items: blog.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.pubDate,
+      pubDate: formatDate(post.data.pubDate),
       description: post.data.description,
       link: `/blog/${post.id}/`,
     })),
