1d9d1972ebc963e8cec3f04115e24294b0082bab
Mikey Sleevi <development@frogman.simplelogin.com>
2026-04-05 14:42:12 -0600

    lint

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -7,13 +7,17 @@ export const IVEELSM_FONTS_FILTER = (filename: string) =>
 export const IVEELSM_FONTS_PRELOAD: PreloadConfig[] = [
 	{
 		filter: (filename: string) =>
-			["ebgaramond-semibold.woff2", "berkeleymonovariable-regular.woff2", "berkeleymonovariable-italic.woff2"].some(
-				(name) => filename.toLowerCase().includes(name),
-			),
+			[
+				"ebgaramond-semibold.woff2",
+				"berkeleymonovariable-regular.woff2",
+				"berkeleymonovariable-italic.woff2",
+			].some((name) => filename.toLowerCase().includes(name)),
 	},
 	{
 		filter: (filename: string) =>
-			["ebgaramond-bold.woff2"].some((name) => filename.toLowerCase().includes(name)),
+			["ebgaramond-bold.woff2"].some((name) =>
+				filename.toLowerCase().includes(name),
+			),
 		media: "(min-width: 641px)",
 	},
 ];
