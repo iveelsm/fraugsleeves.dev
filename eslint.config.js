d535a7d8880ee15b12b6814c5dd85718d3e6328b
Mikey Sleevi <development@frogman.simplelogin.com>
2026-05-19 06:58:46 -0600

    Add stricter type checking (#238)

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,7 @@ export default [
 			perfectionist,
 		},
 		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
 			"perfectionist/sort-imports": [
 				"error",
 				{
--- src/remark-reading-time.ts
+++ src/remark-reading-time.ts
@@ -1,8 +1,9 @@
+import type { Root } from "mdast";
 import { toString } from "mdast-util-to-string";
 import getReadingTime from "reading-time";
 
 export function remarkReadingTime() {
-	return function (tree, { data }) {
+	return function (tree: Root, { data }: { data: Record<string, any> }) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
 		data.astro.frontmatter.minutesRead = readingTime.text;
