3c8525673ac70d36ea8620ae3d203891c9b0dfbe
Mikey Sleevi <development@frogman.simplelogin.com>
2026-05-24 12:23:57 -0600

    Revert "Add early hints for render blocking CSS"
    This reverts commit f47b54ef0831c2a535b31600a868b79102b26ebc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,15 +39,6 @@ export default defineConfig({
 		build: {
 			rollupOptions: {
 				external: ["/pagefind/pagefind.js"],
-				output: {
-					assetFileNames: (assetInfo) => {
-						const hash = process.env.BUILD_HASH;
-						if (hash && assetInfo.names?.some((n) => n.endsWith(".css"))) {
-							return `_astro/[name].${hash}.css`;
-						}
-						return "_astro/[name].[hash][extname]";
-					},
-				},
 			},
 		},
 	},
